c518253d7679823931347c1dd73ebb35575eaf0c
maributt <maributtocks@gmail.com>
2022-11-04 18:14:46 +0100

    Create AddonSynthesisSimple.cs

---
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonSynthesisSimple.cs
@@ -0,0 +1,13 @@
+﻿using FFXIVClientStructs.FFXIV.Client.System.String;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI;
+
+// Client::UI::SynthesisSimpleDialog
+//   Component::GUI::AtkUnitBase
+//     Component::GUI::AtkEventListener
+[StructLayout(LayoutKind.Explicit, Size = 0x288)]
+public unsafe struct AddonSynthesisSimple
+{
+    [FieldOffset(0x0)] public AtkUnitBase AtkUnitBase;
+}
